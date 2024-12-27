ef37884a31624f73314baf8decb85e660d20b897
AUGUSTIN CURINIER <augustin.curinier@gmail.com>
2024-12-28 00:06:22 +0100

    added 3rd solution, more explainable

--- itw/codility3.py
+++ itw/codility3.py
@@ -36,33 +36,70 @@ def solution_optim(S):
     count = 0
     index = len(S) - 1
 
+    # binary representation contains useful info: if the last bit is 1, number is odd, if its 0, number is even.
     # Start from the rightmost bit and process the binary number directly
     while index >= 0:
         if S[index] == '0':
-            # If it's a zero, it's equivalent to a division by 2
+            # If it's a zero, it means the number is even. Corresponds to division by 2 in the algorithm.
             count += 1
             index -= 1
         else:
             # If it's a one:
-            # Case 1: If it's the last bit, just subtract 1 (and we're done, reached 0)
+            # Case 1: If it's the last bit, just subtract 1 (and we're done, reached 0).
             if index == 0:
                 count += 1
                 break
-            # Case 2: Otherwise, perform subtract 1 followed by a division by 2
+            # Case 2: Otherwise, we have an odd number. We
             while index >= 0 and S[index] == '1':
                 count += 1  # Subtract 1
                 index -= 1
-            count += 1  # Division by 2
+                # Add 1 for the division after turning trailing ones into zero
+                if index >= 0:  # Accounting for the division after we made the number even
+                    count += 1
+
+    return count
+
+# memory optimized solution, that does not convert the number to decimal
+def solution_optim2(S):
+    count = 0
+    index = len(S) - 1
+
+    # Convert the binary string to a list for easy modification
+    S = list(S)
+
+    # binary representation contains useful info: if the last bit is 1, number is odd, if its 0, number is even.
+    # Start from the rightmost bit and process the binary number directly
+    while index >= 0:
+        # even number
+        if S[index] == '0':
+            # If it's a zero, it means the number is even. Corresponds to division by 2 in the algorithm:
+            # we shift the index to the left. ---> '1110' = 14 -> '111' is 7
+            count += 1
+            index -= 1
+
+        # odd number
+        else:
+            # Case 1: If it's the last bit, just subtract 1 (and we're done, reached 0).
+            if index == 0:
+                count += 1
+                break
+            # Case 2: Otherwise, we have an odd number. We subsract 1 by flipping the bit to 0, giving an even number.
+            while index >= 0 and S[index] == '1':
+                count += 1  # Subtract 1
+                S[index] = '0'
 
     return count
 
 
 if __name__ == '__main__':
 
-    #S = '1000'
+    # = "1110"
     S = ''
+
+    # test if implementation is memory efficient
     for i in range(400000):
 
         S += '1'
 
     print(solution_optim(S))
+    #print(solution(S))
