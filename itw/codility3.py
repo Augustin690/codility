434ffcc041e018e96e7b5bf5e3d5341c9d4100d8
AUGUSTIN CURINIER <augustin.curinier@gmail.com>
2024-12-27 22:58:05 +0100

    added description

--- itw/codility3.py
+++ itw/codility3.py
@@ -1,5 +1,6 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+"""Given the binary representation in the form of a string containing only 0s and 1s,
+return the number of steps to reduce it to 0 using a Syracuse-like algorithm: if it is even,
+divide it by 2, if it is odd, substract 1, until u reach 0."""
 
 def solution(S):
     # Implement your solution here
@@ -14,6 +15,7 @@ def solution(S):
             V += 2**read_index
         read_index -= 1
 
+    print(f"number = {V}")
     count = 0
     while V > 0 :
         if V % 2 == 0 :
@@ -26,9 +28,9 @@ def solution(S):
 
 if __name__ == '__main__':
 
-    S = ''
-    for i in range(400000):
+    S = '1000'
+    #for i in range(400000):
 
-        S += '1'
+   #     S += '1'
 
     print(solution(S))
